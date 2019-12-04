6186aa51a0398855925e7c8ebf3bf3cba3733687
Rolf Kreibaum <rolf.kreibaum@posteo.de>
2019-12-04 07:50:56 +0100

    Add header to library, fuel part.

--- library.jl
+++ library.jl
@@ -1,5 +1,9 @@
 
 
+################################################################################
+# Fuel requirements ############################################################
+################################################################################
+
 """Fuel required to launch a given module is based on its mass.
 Specifically, to find the fuel required for a module, take its mass,
 divide by three, round down, and subtract 2.
